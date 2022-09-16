bbf721629effafd889982599057fcf426246af52
Harsger <herrmannmaximilian@web.de>
2022-09-16 16:52:40 +0200

    housekeeping visualization using PyQtGraph

---
+++ plotHousekeeping.py
@@ -0,0 +1,324 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import copy
+import math
+import time
+from datetime import datetime
+import numpy as np
+from pyqtgraph.Qt import QtGui, QtCore
+import pyqtgraph as pg
+#from scipy.optimize import curve_fit
+#from scipy import odr
+
+secondsPER = {
+    ""       :        1 , #
+    "s"      :        1 , #
+    "second" :        1 , #
+    "m"      :       60 , #
+    "minute" :       60 , #
+    "h"      :     3600 , #            60 * 60
+    "hour"   :     3600 , #            60 * 60
+    "d"      :    86400 , #       24 * 60 * 60
+    "day"    :    86400 , #       24 * 60 * 60
+    "w"      :   604800 , #   7 * 24 * 60 * 60
+    "week"   :   604800 , #   7 * 24 * 60 * 60
+    "M"      :  2678400 , #   31* 24 * 60 * 60
+    "month"  :  2678400 , #   31* 24 * 60 * 60
+    "y"      : 31536000 , # 365 * 24 * 60 * 60
+    "year"   : 31536000 , # 365 * 24 * 60 * 60
+}
+
+class TimeAxisItem( pg.AxisItem ) :
+    def __init__( self , *args , **kwargs ) :
+        super( TimeAxisItem , self ).__init__( *args , **kwargs )
+    def tickStrings( self , values , scale , spacing ) :
+        global secondsPER
+        ticks = []
+        if not values :
+            return []
+        milliseconds = False
+        if   spacing >= secondsPER[  "year"] * 3 :
+            timeFormat = "%Y"
+        #elif spacing >= secondsPER[ "month"] * 3 :
+            #timeFormat = "%d.%m.%Y"    
+        elif spacing >= secondsPER[   "day"] * 3 :
+            timeFormat = "%d.%m."
+        #elif spacing >= secondsPER[  "hour"] * 3 :
+            #timeFormat = "%H:%M"
+        elif spacing >= secondsPER["minute"] * 3 :
+            timeFormat = "%H:%M"
+        elif spacing >= 3 :
+            timeFormat = "%H:%M:%S"
+        else:
+            timeFormat = "%H:%M:%S.%f"
+            milliseconds = True
+        for v in values:
+            try:
+                if milliseconds:
+                    ticks.append(
+                        datetime.fromtimestamp(v)
+                                .strftime(timeFormat).rstrip("0")+"\""
+                    )
+                else:
+                    ticks.append( 
+                        datetime.fromtimestamp(v).strftime(timeFormat) 
+                    )
+            except ValueError:
+                ticks.append( "" )
+        return ticks
+
+parameters = {
+                "dataFile"         : None                ,
+                "parameterFile"    : None                ,
+                "dateNtimeFormat"  : "%Y-%m-%dT%H:%M:%S" ,
+                "timeColumn"       : 0                   ,
+                "valueColumn"      : 3                   ,
+                "specifierColumns" : [ 2 , 1 , 4 ]       ,
+                "markerSize"       : 1.
+            }
+
+specifiers = []
+timeInput = []
+quantityUnits = {}
+plots = {}
+data = []
+timeRange = []
+
+def readParameterInput( argv ) :
+    global parameters , specifiers , timeInput , plots
+    argc = len( argv )
+    if argc < 2 :
+        print(" ERROR : at least two arguments required ")
+        print("         -> data-file-name and parameter-file-name ")
+        sys.exit(1)
+    parameters["dataFile"]      = argv[0]
+    parameters["parameterFile"] = argv[1]
+    if argc > 2 :
+        timeInput.append( argv[2] )
+    if argc > 3 :
+        timeInput.append( argv[3] )
+    with open(parameters["parameterFile"],'r') as parameterFile:
+        line = parameterFile.readline()
+        while line:
+            words = line.split()
+            line = parameterFile.readline()
+            wordCount = len(words)
+            if( wordCount < 1 or words[0].startswith("#") ) : continue
+            if( words[0] == "PARAMETER" and wordCount > 2 ) :
+                parameters[ words[1] ] = words[2]
+                continue
+            if( words[0] == "PLOT" and wordCount > 1 ) :
+                singlePlot = []
+                for w in range( 2 , wordCount ) :
+                    if( words[w] != "%" ) :
+                        singlePlot.append( words[w] )
+                    else :
+                        singlePlot.append( "" )
+                plots[ words[1] ] = singlePlot
+                continue
+            if( wordCount > 1 ) :
+                specs = [ words[0] , words[1] ]
+                if( wordCount > 2 and words[2] != "%" ) :
+                    specs.append( words[2] )
+                else :
+                    specs.append( "" )
+                for w in range( 3 , wordCount ) :
+                    if words[w] == "%" :
+                        specs.append( "" )
+                    else :
+                        specs.append( words[w] )
+                specifiers.append( specs )
+    if( 
+        parameters["dataFile"] == None
+        or parameters["parameterFile"] == None
+        or len( specifiers ) < 1
+    ) :
+        print(" ERROR : input not well specified ")
+        sys.exit(2)
+
+def readData() :
+    global parameters , specifiers , data , timeInput , timeRange
+    rawData = np.loadtxt( parameters["dataFile"] , dtype='U' )
+    if rawData.shape[0] < 1 :
+        print(" ERROR : data empty ")
+        sys.exit(3)
+    print(" data : "+str( rawData.shape[0] ) )
+    swapValues = False
+    startColumn = int( parameters["timeColumn"] )
+    endColumn   = int( parameters["valueColumn"] )
+    timeRange.append( np.min( rawData[:,startColumn].astype(float) ) )
+    timeRange.append( np.max( rawData[:,startColumn].astype(float) ) )
+    print(
+            " time : "
+            +str( datetime.fromtimestamp( timeRange[0] )
+                          .strftime( parameters["dateNtimeFormat"] )
+                )
+            +" to "
+            +str( datetime.fromtimestamp( timeRange[1] )
+                          .strftime( parameters["dateNtimeFormat"] )
+                )
+        )
+    if len( timeInput ) == 2 :
+        for t in range(2) :
+            timeRange[t] = ( 
+                                datetime.strptime( 
+                                    timeInput[t] ,
+                                    parameters["dateNtimeFormat"]
+                                )
+                                - 
+                                datetime( 1970 , 1 , 1 , 1 ) # MEZ
+                            ).total_seconds()
+    if startColumn > endColumn :
+        swapValues = True
+        startColumn , endColumn = endColumn , startColumn
+    columnStride = endColumn - startColumn
+    endColumn = endColumn+1
+    for spec in specifiers :
+        equalTOspecifier = np.array(
+                                rawData[
+                                    : , int(parameters["specifierColumns"][0])
+                                ]
+                                ==
+                                spec[0]
+                            )
+        equalTOquantity  = np.array(
+                                rawData[
+                                    : , int(parameters["specifierColumns"][1])
+                                ] 
+                                == 
+                                spec[1]
+                            )
+        timeNvalue       = rawData[
+                                np.logical_and(
+                                                equalTOspecifier ,
+                                                equalTOquantity
+                                    ) ,
+                                startColumn:endColumn:columnStride
+                            ].astype(float)
+        if swapValues :
+            timeNvalue[:,[1,0]] = timeNvalue[:,[0,1]]
+        print(
+                " "+str(spec[0])+
+                " "+str(spec[1])+
+                " \t "+str( timeNvalue.shape[0] )
+            )
+        data.append( np.swapaxes( timeNvalue , 0 , 1 ) )
+        units = rawData[ 
+                            equalTOquantity , 
+                            int( parameters["specifierColumns"][2] )
+                        ]
+        if units.shape[0] > 0 :
+            quantityUnits[ spec[1] ] = units[0]
+        else :
+            quantityUnits[ spec[1] ] = ""
+
+def main(argv):
+    global parameters , specifiers , timeInput , plots , data , timeRange
+    global secondsPER
+    readParameterInput( argv )
+    readData()
+    app = QtGui.QApplication([])
+    pg.setConfigOption('background', 'w')
+    pg.setConfigOption('foreground', 'k')
+    win = pg.GraphicsLayoutWidget()
+    drawnINplot = []
+    for spec in specifiers :
+        drawnINplot.append( -1 )
+    plotCount = 0
+    plotMap = {}
+    plotNumber = {}
+    for p in plots :
+        toFill = False
+        quantity = ""
+        for spec in specifiers :
+            if spec[2] == p :
+                quantity = spec[1]
+                toFill = True
+                break
+        if not toFill : continue
+        plotNumber[p] = plotCount
+        plotMap[p] = win.addPlot( 
+            row = plotCount , col = 0
+            , axisItems = { "bottom" : TimeAxisItem( orientation = 'bottom' ) } 
+        )
+        #plotMap[p].getAxis('bottom').hide()
+        #plotMap[p].vb.addItem( TimeAxisItem( orientation = 'bottom' ) )
+        plotCount += 1
+        for c , spec in enumerate( specifiers ) :
+            if spec[2] == p :
+                drawnINplot[c] = plotNumber[p]
+                symbolPen = 'k'
+                symbol    = 'o'
+                if len( spec ) > 3 and spec[3] != "" :
+                    symbolPen = spec[3]
+                    if str( spec[3] ).isdigit() :
+                        symbolPen = int( spec[3] )
+                if len( spec ) > 4 and spec[4] != "" :
+                    symbol    = spec[4]
+                plotMap[p].plot( 
+                                data[c][0] , data[c][1] ,
+                                name        = str( spec[0] ) ,
+                                pen         = None ,
+                                symbolBrush = symbolPen ,
+                                symbolPen   = None ,
+                                symbol      = symbol ,
+                                symbolSize  = float( parameters["markerSize"] )
+                            )
+        unit = quantityUnits[ quantity ]
+        if len( plots[p] ) > 1 :
+            unit = plots[p][1]
+        plotMap[p].setLabel( 'left' , str(plots[p][0]) , units=str(unit) )
+        if len( plots[p] ) > 3 and plots[p][2] != "" and plots[p][3] != "" :
+            plotMap[p].setYRange( float( plots[p][2] ) , float( plots[p][3] ) )
+        if len( plots[p] ) > 4 and plots[p][4] == "log" :
+            plotMap[p].setLogMode( x = False , y = True )
+    for c , spec in enumerate( specifiers ) :
+        if drawnINplot[c] > -1 : continue
+        if spec[1] not in plotNumber :
+            plotNumber[ spec[1] ] = plotCount
+            plotMap[ spec[1] ] = win.addPlot( 
+                row = plotCount , col = 0 , 
+                axisItems ={ "bottom" : TimeAxisItem( orientation = 'bottom' ) } 
+            )
+            plotCount += 1
+        drawnINplot[c] = plotNumber[ spec[1] ]
+        symbolPen = 'k'
+        symbol    = 'o'
+        if len( spec ) > 3 and spec[3] != "" :
+            symbolPen = spec[3]
+        if len( spec ) > 4 and spec[4] != "" :
+            symbol    = spec[4]
+        plotMap[ spec[1] ].plot( 
+                                data[c][0] , data[c][1] ,
+                                name        = str( spec[0] ) ,
+                                pen         = None ,
+                                symbolBrush = symbolPen ,
+                                symbolPen   = None ,
+                                symbol      = symbol ,
+                                symbolSize  = float( parameters["markerSize"] )
+                            )
+        plotMap[ spec[1] ].setLabel( 
+                                        'left' , str( spec[1] ) ,
+                                        str( quantityUnits[ spec[1] ] ) 
+                                    )
+    for p in plotMap :
+        plotMap[p].setXRange( timeRange[0] , timeRange[1] )
+        plotMap[p].showGrid( x = True , y = True )
+        if plotNumber[p] == plotCount-1 :
+            timeFormat = "%d.%m.%Y"
+            if timeRange[1] - timeRange[0] > secondsPER["day"] * 3. :
+                timeFormat = "%Y"
+            centerTime = timeRange[0] + 0.5 * ( timeRange[1] - timeRange[0] )
+            axisTitle = str( 
+                                datetime.fromtimestamp( centerTime )
+                                        .strftime(      timeFormat )
+                            )
+            plotMap[p].setLabel( 'bottom' , axisTitle , units="" )
+    win.resize( 800 , plotCount * 200 )
+    win.show()
+    if ( sys.flags.interactive != 1 ) or not hasattr( QtCore, 'PYQT_VERSION' ) :
+        QtGui.QApplication.instance().exec_()
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
